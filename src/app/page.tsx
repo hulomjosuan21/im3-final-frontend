0b5ddb0e02c493d56cae99aba961fc0cc1dbfab9
Josuan Leonardo Hulom <hulomjosuan@gmail.com>
2025-04-27 13:03:46 +0800

    fixing no use fills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { useFixViewportHeight } from "@/hooks/useFixViewportHeight";
 import dynamic from "next/dynamic";
 import LoadingComponent from "@/components/loading-component";
-import useServiceWorker from "@/hooks/useServiceWorker";
 
 const Onboarding = dynamic(() => import("@/components/onBoarding"), {
   ssr: false,
@@ -12,6 +11,5 @@ const Onboarding = dynamic(() => import("@/components/onBoarding"), {
 
 export default function PredictPage() {
   useFixViewportHeight();
-  useServiceWorker();
   return <Onboarding />;
 }
